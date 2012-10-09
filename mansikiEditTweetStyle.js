8c8b8df93f1e0ab0702fa223eecca734a1162ec7
ryunosinfx <ryunosinfx@livedoor.com>
2012-10-09 22:44:53 +0900

    つうぃったースタイル始めました5

--- mansikiEditTweetStyle.js
+++ mansikiEditTweetStyle.js
@@ -4,11 +4,10 @@ var MansikiModeConst={
 
 }
 
-
-
 var MansikiTweetStyleEditor= function(id, width,height,ancer){
 	this.doc;
 	this.tweets={};
+	this.tweetsFuncs={};
 	this.tweetsSort=[];
 	this.state={selected:undefined};
 	this.cursor=0;
@@ -19,18 +18,34 @@ var MansikiTweetStyleEditor= function(id, width,height,ancer){
 		,tweetAreaHeight:72
 		,modAdd:"追加"
 		,modUpdate:"更新"
-	}
+	};
 	this.cmdButtonsState={};
 	this.commands=[{id:"page",data:{}}];
 	this.currentCmd="page";
 	this.tweetBoxParent = $("#TWtweetBoxParent");
 	this.scrollTimer;
 	$(document).bind("scroll",{self:this},this.onScroll);
+	this.funcs=new ManikiFunctions(this,0);
+	this.funcs.add2Funcs(new ManikiFuncPage(this,0));
+	this.funcs.add2Funcs(new ManikiFuncKoma(this,0));
+	this.funcs.add2Funcs(new ManikiFuncFukidashi(this,0));
+	this.funcs.add2Funcs(new ManikiFuncSetting(this,0));
+	this.funcs.add2Funcs(new ManikiFuncActor(this,0));
+	this.funcs.add2Funcs(new ManikiFuncObject(this,0));
+	this.funcs.add2Funcs(new ManikiFuncBackground(this,0));
+	this.funcs.add2Funcs(new ManikiFuncSound(this,0));
+	this.funcs.add2Funcs(new ManikiFuncEffect(this,0));
+	this.funcs.add2Funcs(new ManikiFuncNalation(this,0));
+	this.funcs.add2Funcs(new ManikiFuncQuote(this,0));
+	this.funcs.add2Funcs(new ManikiFuncSean(this,0));
+	this.funcs.add2Funcs(new ManikiFuncFukusen(this,0));
+	this.funcs.makeInputArea();
+	this.currentFuncId="";
 }
 MansikiTweetStyleEditor.prototype={
 	init:function(){
 		this.viewList = $("#TMtweetList");
-		this.tweetArea= $("#TMtweetTextarea");
+		this.bredgeArea= $("#TMtweetInput");
 		this.addButton= $("#TMadd");
 		this.clearButton= $("#TMclear");
 		this.twCmdAreaUpper = $("#TWcmdAreaUpper");
@@ -41,9 +56,8 @@ MansikiTweetStyleEditor.prototype={
 	initBinds:function(me){
 		me.addButton.bind("click",{self:me},me.addTweet);
 		me.clearButton.bind("click",{self:me},me.clearTweet);
-		me.tweetArea.bind("keyup",{self:me},me.autoResize);
 		$("#TMTweetMode").text(me.constMap.modAdd);
-		this.cmdButtonsHilightInit();
+		me.cmdButtonsHilightInit();
 	}, 
 	cmdButtonsHilightInit:function(){
 		var bottons = $(".commands div");
@@ -80,10 +94,29 @@ MansikiTweetStyleEditor.prototype={
 		$("#"+id).css("border-color",buttonState["background-color"]);
 		me.twCmdAreaUpper.css("background-color",buttonState["background-color"]);
 		me.twCmdAreaUnder.css("background-color",buttonState["background-color"]);
+		me.currentFuncId =id;
 	},
+	//--------------------------------------------------------------------
+	buildFuncs:function(me,idIndex){
+		var func = me.funcs.getFunc(me.currentFuncId);
+		func.text=me.tweets[idIndex];
+		me.tweetsFuncs[idIndex]=func;
+	},
+	updateFuncs:function(me,idIndex){
+		var func = me.tweetsFuncs[idIndex];
+		var nowFunc = me.funcs.getFunc(me.currentFuncId);
+		if(func===undefined || func.Id!==nowFunc.Id){
+			delete me.tweetsFuncs[idIndex];
+			me.tweetsFuncs[idIndex] = nowFunc;
+			return true;
+		}
+		return false;
+	},
+	//--------------------------------------------------------------------
 	addTweet:function(event){
 		var me= event.data.self;
-		var text = me.tweetArea.val();
+		me.funcs.addTweet();
+		var text = me.bredgeArea.val();
 		if(text!==undefined && text.length>0){
 			event.data.text=text;
 			if(me.state.selected === undefined){//add
@@ -92,7 +125,8 @@ MansikiTweetStyleEditor.prototype={
 				me.insertTweet(event);
 console.log("addTweet idIndex:"+event.data.idIndex+"/me.cursor:"+me.cursor+"/me.tweetIdMap:"+me.tweetIdMap.toSource());
 				me.cursor++;
-				me.tweetArea.val("").focus();
+				me.bredgeArea.val("");
+				me.funcs.clearTweet();
 				me.showCursor(me);
 			}else{//update
 				event.data.idIndex=me.state.selected;
@@ -103,6 +137,10 @@ console.log("addTweet idIndex:"+event.data.idIndex+"/me.cursor:"+me.cursor+"/me.
 	insertTweet:function(event){
 		var me= event.data.self;
 		var tweetBox=me.buildTweetBox(event);
+		me.doInsertTweet(me,tweetBox);
+		me.initViewCursorObj({data:{self:me,idIndex:me.tweetIdMap[me.cursor],offsetY:999}});
+	},
+	doInsertTweet:function(me,tweetBox){
 		if(me.cursor*1===0){
 			if(me.viewList.children("div").length > 0){
 				me.viewList.children("div").eq(0).before(tweetBox);
@@ -114,11 +152,6 @@ console.log("addTweet idIndex:"+event.data.idIndex+"/me.cursor:"+me.cursor+"/me.
 			var preId=me.constMap.tweetIdPrefix+idIndex;
 			$("#"+preId).after(tweetBox);
 		}
-		me.autoResize(event);
-		me.initViewCursorObj({data:{self:me,idIndex:me.tweetIdMap[me.cursor],offsetY:999}});
-	},
-	getFormatedTextCRLF:function(text){
-		return text.replace(/(\r|\n|\r\n)/g, "<br />");
 	},
 	buildTweetBox:function(event){
 		var me= event.data.self;
@@ -126,6 +159,7 @@ console.log("addTweet idIndex:"+event.data.idIndex+"/me.cursor:"+me.cursor+"/me.
 		var idIndex= event.data.idIndex;
 		me.tweetIdMap = MansikiMapUtil.after(me.tweetIdMap,me.cursor,idIndex);
 		me.tweets[idIndex]=text;
+		me.buildFuncs(me,idIndex);
 		return me.execBuildTweetBox(me,idIndex);
 	},
 	execBuildTweetBox:function(me,idIndex){
@@ -138,9 +172,11 @@ console.log("execBuildTweetBox idIndex:"+idIndex+"/me.tweets:"+me.tweets.toSourc
 		var twbButtonMoveUp= $("<div class='tweetBoxCmdButton'>▲</div>");
 		var twbButtonMoveDown= $("<div class='tweetBoxCmdButton'>▼</div>");
 		var twbButtonUnite= $("<div class='tweetBoxCmdButton'>＋</div>");
-		var tweetBoxtext = $("<div class='tweetBoxText'>"+me.getFormatedTextCRLF(text)+"</div>");
+		var tweetBoxtext = $("<div class='tweetBoxText'>"+MansikiMapUtil.getFormatedTextCRLF(text)+"</div>");
+		var tweetBoxSlot= $("<div class='tweetSlot'></div>");
 		tweetBox.append(tweetBoxButtons);
 		tweetBox.append(tweetBoxtext);
+		tweetBox.append(tweetBoxSlot);
 		tweetBoxButtons.append(twbButtonMoveDown).append(twbButtonMoveUp).append(twbButtonDel).append(twbButtonUnite);
 		tweetBoxtext.bind("click",{self:me,idIndex:idIndex},me.loadTweet);
 		twbButtonDel.bind("click",{self:me,idIndex:idIndex},me.deleteTweet);
@@ -150,6 +186,11 @@ console.log("execBuildTweetBox idIndex:"+idIndex+"/me.tweets:"+me.tweets.toSourc
 		tweetBox.bind("click",{self:me,idIndex:idIndex,offsetY:20},me.initViewCursorObj);
 		tweetBox.bind("mouseover",{self:me,idIndex:idIndex},me.showCmdBox);
 		tweetBox.bind("mouseout",{self:me,idIndex:idIndex},me.hideCmdBox);
+		var func = me.tweetsFuncs[idIndex];
+		if(func!==undefined){
+			tweetBox.css("background-color",func.color);
+		}
+		func.addIndentClass(tweetBox);
 		return tweetBox;
 	},
 	loadTweet:function(event){
@@ -159,13 +200,15 @@ console.log("execBuildTweetBox idIndex:"+idIndex+"/me.tweets:"+me.tweets.toSourc
 		var tweetBox=$("#"+id);
 		for(var cursor in me.tweetIdMap){
 			var tmpId = me.constMap.tweetIdPrefix+me.tweetIdMap[cursor];
-			$("#"+tmpId).css("border-left-width","0px");
+			$("#"+tmpId).css("box-shadow","0px");
 		}
-		tweetBox.css("border-left-width","20px");
+		tweetBox.css("box-shadow","20px");
 		var text= me.tweets[idIndex];
-		me.tweetArea.val(text);
+		var func = me.tweetsFuncs[idIndex];
+		me.bredgeArea.val(text);
+		me.funcs.loadTweet();
+		me.cmdButtonsHilight({data:{self:me,id:func.getFullId()}});
 		me.state.selected = idIndex;
-		me.autoResize(event);
 		$("#TMTweetMode").text(me.constMap.modUpdate);
 		me.initViewCursorObj({data:{self:me,idIndex:idIndex,offsetY:165}});
 	},
@@ -174,14 +217,22 @@ console.log("execBuildTweetBox idIndex:"+idIndex+"/me.tweets:"+me.tweets.toSourc
 		var text= event.data.text;
 		var idIndex = event.data.idIndex;
 		me.tweets[idIndex] =text;
+		var isTweetBoxUpdated = me.updateFuncs(me,idIndex);
 		var id=me.constMap.tweetIdPrefix+idIndex;
 		var tweetBox=$("#"+id);
-		tweetBox.children(".tweetBoxText").eq(0).html(me.getFormatedTextCRLF(text));
+		if(isTweetBoxUpdated === false){
+			tweetBox.children(".tweetBoxText").eq(0).html(MansikiMapUtil.getFormatedTextCRLF(text));
+		}else{
+			tweetBox.remove();
+			tweetBox = me.execBuildTweetBox(me,idIndex);
+			me.doInsertTweet(me,tweetBox);
+		}
 	},
 	deleteTweet:function(event){
 		var me= event.data.self;
 		var idIndex = event.data.idIndex;
-		me.tweets[idIndex] =undefined;
+		delete me.tweets[idIndex] ;
+		delete me.tweetsFuncs[idIndex];
 console.log("AAA idIndex:"+idIndex+"/me.cursor:"+me.cursor+"/me.tweetIdMap:"+me.tweetIdMap.toSource());
 		if(me.state.selected===idIndex){
 			me.clearTweet(event);
@@ -269,11 +320,11 @@ console.log("idIndex:"+idIndex+"/subject:"+subject+"/direct:"+direct+"/cursor:"+
 		var me= event.data.self;
 		for(var cursor in me.tweetIdMap){
 			var tmpId = me.constMap.tweetIdPrefix+me.tweetIdMap[cursor];
-			$("#"+tmpId).css("border-left-width","0px");
+			$("#"+tmpId).css("box-shadow","0px");
 		}
 		me.state.selected = undefined;
-		me.tweetArea.val("");
-		me.autoResize(event);
+		me.bredgeArea.val("");
+		me.funcs.clearTweet();
 		$("#TMTweetMode").text(me.constMap.modAdd);
 		me.initViewCursorObj({data:{self:me,idIndex:me.tweetIdMap[me.cursor],offsetY:20}});
 	},
@@ -306,15 +357,6 @@ console.log("idIndex:"+idIndex+"/subject:"+subject+"/direct:"+direct+"/cursor:"+
 	},
 	changeObject:function(event){
 		
-	},
-	autoResize:function(event){
-		var me= event.data.self;
-		var textareaObj = me.tweetArea.get(0);
-		 me.tweetArea.css("height",me.constMap.tweetAreaHeight);
-		var height = textareaObj.scrollHeight+12;
-		if(me.constMap.tweetAreaHeight< height){
-		 	me.tweetArea.css("height",height);
-		}
 	},
 	calcStringLength:function(event){
 		var me= event.data.self;
@@ -421,18 +463,316 @@ console.log("del tmpCursor:"+newIndex+":"+tmpVal+"/targetIndex:"+targetIndex);
 				return key;
 			}
 		}
+	},
+	getFormatedTextCRLF:function(text){
+		return text.replace(/(\r|\n|\r\n)/g, "<br />");
 	}
-
-
 }
 
-var ManikiFunctions=function(){
+var ManikiFunctions=function(editor,idIndex){
+	this.editor = editor;
 	this.frame="";
-
+	this.nameLc="日本語名";
+	this.nameEn="EnglishName";
+	this.idIndex=idIndex;
+	this.color="#35FFDA";
+	this.idPrefix ="add";
+	this.idSufix ="Icon";
+	this.text="";
+	this.Funcs={};
+	this.inputArea = $("#TMinputArea");
+	this.tweetArea = $("#TMtweetTextarea");
+	this.bridgeArea = $("#TMtweetInput");
+	this.level=1;
+	this.indentClassPrefix="indent";
 }
 
 ManikiFunctions.prototype={
+	add2Funcs:function(func){
+		this.Funcs[this.idPrefix+func.Id+this.idSufix]=func;
+	},
+	getFunc:function(id){
+		if(this.Funcs[id]===undefined){
+			return ;
+		}
+		return this.Funcs[id];
+	},
+	init:function(){
+	
+	},
+	create:function(idIndex){
+		return new ManikiFunctions(idIndex);
+	},
+	autoResize:function(event){
+		var me= event.data.self;
+		var textareaObj = me.tweetArea.get(0);
+		 me.tweetArea.css("height",me.editor.constMap.tweetAreaHeight);
+		var height = textareaObj.scrollHeight+12;
+		if(me.editor.constMap.tweetAreaHeight< height){
+		 	me.tweetArea.css("height",height);
+		}
+	},
+	makeInputArea:function(){
+		this.inputArea.empty();
+		this.tweetArea = $('<textarea class="input" id="TMtweetTextarea"></textarea>');
+		this.inputArea.append(this.tweetArea );
+		this.tweetArea.bind("keyup",{self:this},this.autoResize);
+	},
+	loadTweet:function(){
+		var text = this.bridgeArea.val();
+		this.tweetArea.val(text);
+		this.tweetArea.focus();
+	},
+	addIndentClass:function(tweetBox){
+		tweetBox.addClass(this.indentClassPrefix+this.level);
+	},
+	addTweet:function(){
+		var text = this.tweetArea.val();
+		this.bridgeArea.val(this.convertTextToManikiSyntax(text));
+		this.tweetArea.focus();
+	},
+	convertTextToManikiSyntax:function(text){
+		return text;
+	},
+	getPureText:function(value){
+		return value;
+	},
+	clearTweet:function(text){
+		this.tweetArea.val("").focus();
+		this.autoResize({data:{self:this}});
+	},
+	getFullId:function(){
+		return this.idPrefix+this.Id+this.idSufix;
+	}
+}
+
+var ManikiFuncPage=function(editor,idIndex){
+	ManikiFunctions.apply(this, arguments);
+	this.nameLc="頁";
+	this.nameEn="Page";
+	this.color="#35FFDA";
+	this.Id ="Page";
+	this.level =1;
+}
+ManikiFuncPage.prototype = new ManikiFunctions();
+//ManikiFuncPage.prototype.
+ManikiFuncPage.prototype.create=function(idIndex){
+	return new ManikiFuncPage(idIndex);
+}
+
+var ManikiFuncKoma=function(editor,idIndex){
+	ManikiFunctions.apply(this, arguments);
+	this.nameLc="コマ";
+	this.nameEn="koma";
+	this.color="#33FF7E";
+	this.Id ="Koma";
+	this.level =2;
 
 }
+ManikiFuncKoma.prototype = new ManikiFunctions();
+//ManikiFuncPage.prototype.
+ManikiFuncKoma.prototype.create=function(idIndex){
+	return new ManikiFuncKoma(idIndex);
+}
+
+
+var ManikiFuncFukidashi=function(editor,idIndex){
+	ManikiFunctions.apply(this, arguments);
+	this.nameLc="噴き出し";
+	this.nameEn="Baloon";
+	this.color="#33FF3D";
+	this.Id ="Fukidashi";
+	this.level =3;
+}
+ManikiFuncFukidashi.prototype = new ManikiFunctions();
+//ManikiFuncPage.prototype.
+ManikiFuncFukidashi.prototype.create=function(idIndex){
+	return new ManikiFuncFukidashi(idIndex);
+}
+
+
+var ManikiFuncSetting=function(editor,idIndex){
+	ManikiFunctions.apply(this, arguments);
+	this.nameLc="設定";
+	this.nameEn="Settings";
+	this.color="#7FFF35";
+	this.Id ="Setting";
+	this.level =3;
+}
+ManikiFuncSetting.prototype = new ManikiFunctions();
+//ManikiFuncSetting.prototype.
+ManikiFuncSetting.prototype.create=function(idIndex){
+	return new ManikiFuncSetting(idIndex);
+}
+
+
+var ManikiFuncActor=function(editor,idIndex){
+	ManikiFunctions.apply(this, arguments);
+	this.nameLc="役者";
+	this.nameEn="Actor";
+	this.color="#CDFF34";
+	this.Id ="Man";
+	this.level =3;
+}
+ManikiFuncActor.prototype = new ManikiFunctions();
+//ManikiFuncActor.prototype.
+ManikiFuncActor.prototype.create=function(idIndex){
+	return new ManikiFuncActor(idIndex);
+}
+
+
+var ManikiFuncObject=function(editor,idIndex){
+	ManikiFunctions.apply(this, arguments);
+	this.nameLc="小道具";
+	this.nameEn="Object";
+	this.color="#FFEF34";
+	this.Id ="Object";
+	this.level =3;
+}
+ManikiFuncObject.prototype = new ManikiFunctions();
+//ManikiFuncObject.prototype.
+ManikiFuncObject.prototype.create=function(idIndex){
+	return new ManikiFuncObject(idIndex);
+}
+
+
+var ManikiFuncBackground=function(editor,idIndex){
+	ManikiFunctions.apply(this, arguments);
+	this.nameLc="背景";
+	this.nameEn="Background";
+	this.color="#FFBF34";
+	this.Id ="Background";
+	this.level =3;
+}
+ManikiFuncBackground.prototype = new ManikiFunctions();
+//ManikiFuncBackground.prototype.
+ManikiFuncBackground.prototype.create=function(idIndex){
+	return new ManikiFuncBackground(idIndex);
+}
+
+
+var ManikiFuncSound=function(editor,idIndex){
+	ManikiFunctions.apply(this, arguments);
+	this.nameLc="擬音";
+	this.nameEn="Sound";
+	this.color="#FFBF34";
+	this.Id ="Sound";
+	this.level =3;
+}
+ManikiFuncSound.prototype = new ManikiFunctions();
+//ManikiFuncSound.prototype.
+ManikiFuncSound.prototype.create=function(idIndex){
+	return new ManikiFuncSound(idIndex);
+}
+
+
+var ManikiFuncEffect=function(editor,idIndex){
+	ManikiFunctions.apply(this, arguments);
+	this.nameLc="効果";
+	this.nameEn="Effect";
+	this.color="#FF7E34";
+	this.Id ="Effect";
+}
+ManikiFuncEffect.prototype = new ManikiFunctions();
+//ManikiFuncEffect.prototype.
+ManikiFuncEffect.prototype.create=function(idIndex){
+	return new ManikiFuncEffect(idIndex);
+}
+
+
+var ManikiFuncNalation=function(editor,idIndex){
+	ManikiFunctions.apply(this, arguments);
+	this.nameLc="説明";
+	this.nameEn="Settings";
+	this.color="#30FF64";
+	this.Id ="Nalation";
+	this.level =3;
+}
+ManikiFuncNalation.prototype = new ManikiFunctions();
+//ManikiFuncNalation.prototype.
+ManikiFuncNalation.prototype.create=function(idIndex){
+	return new ManikiFuncNalation(idIndex);
+}
+
+
+var ManikiFuncQuote=function(editor,idIndex){
+	ManikiFunctions.apply(this, arguments);
+	this.nameLc="注釈";
+	this.nameEn="Quote";
+	this.color="#FF337A";
+	this.Id ="Quote";
+	this.level =3;
+}
+ManikiFuncQuote.prototype = new ManikiFunctions();
+//ManikiFuncQuote.prototype.
+ManikiFuncQuote.prototype.create=function(idIndex){
+	return new ManikiFuncQuote(idIndex);
+}
+
+
+var ManikiFuncSean=function(editor,idIndex){
+	ManikiFunctions.apply(this, arguments);
+	this.nameLc="場所";
+	this.nameEn="Sean";
+	this.color="#FF31F5";
+	this.Id ="Sean";
+	this.level =3;
+}
+ManikiFuncSean.prototype = new ManikiFunctions();
+//ManikiFuncSean.prototype.
+ManikiFuncSean.prototype.create=function(idIndex){
+	return new ManikiFuncSean(idIndex);
+}
+
+
+var ManikiFuncFukusen=function(editor,idIndex){
+	ManikiFunctions.apply(this, arguments);
+	this.nameLc="伏線";
+	this.nameEn="Settings";
+	this.color="#BA2FFF";
+	this.Id ="Fukusen";
+	this.level =3;
+}
+ManikiFuncFukusen.prototype = new ManikiFunctions();
+//ManikiFuncFukusen.prototype.
+ManikiFuncFukusen.prototype.create=function(idIndex){
+	return new ManikiFuncFukusen(idIndex);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
